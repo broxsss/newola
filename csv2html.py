005e942b385d032833fdb27fd2c5390eb14a6562
Akshay Saini <akshay.saini@C02QD5PGG8WP.local>
2019-05-23 17:18:10 +0530

    updayed csv2html python script

--- csv2html.py
+++ csv2html.py
@@ -1,14 +1,14 @@
 import pandas as pd 
 import codecs
-df = pd.read_csv("/Users/akshay.saini/.jenkins/workspace/new/newola/RecordingTestPlan.csv")
-df.to_html('/Users/akshay.saini/.jenkins/workspace/new/newola/RecordingTestPlan.html')
-fn=codecs.open("/Users/akshay.saini/.jenkins/workspace/new/newola/RecordingTestPlan.html", 'r')
+df = pd.read_csv("$JENKINS_HOME/workspace/new/newola/RecordingTestPlan.csv")
+df.to_html('$JENKINS_HOME/workspace/new/newola/RecordingTestPlan.html')
+fn=codecs.open("$JENKINS_HOME/workspace/new/newola/RecordingTestPlan.html", 'r')
 m = fn.read()
 #print (m)
-f = open("/Users/akshay.saini/.jenkins/workspace/new/newola/index.html",'r')
+f = open("$JENKINS_HOME/workspace/new/newola/index.html",'r')
 filedata = f.read() # read contents
 f.close() # closes file
 filedata = filedata.replace("replaceit2", m)
-f = open("/Users/akshay.saini/.jenkins/workspace/new/newola/index.html",'w')
+f = open("$JENKINS_HOME/workspace/new/newola/index.html",'w')
 f.write(filedata)
 f.close()
