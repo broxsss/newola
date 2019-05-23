a48740f3857f026580c8af30ca793386d76c85e3
Akshay Saini <akshay.saini@C02QD5PGG8WP.local>
2019-05-23 18:24:41 +0530

    updayed csv2html python script n

--- csv2html.py
+++ csv2html.py
@@ -1,14 +1,19 @@
+from pathlib import Path
 import pandas as pd 
 import codecs
-df = pd.read_csv("$JENKINS_HOME/workspace/new/newola/RecordingTestPlan.csv")
-df.to_html('$JENKINS_HOME/workspace/new/newola/RecordingTestPlan.html')
-fn=codecs.open("$JENKINS_HOME/workspace/new/newola/RecordingTestPlan.html", 'r')
+import os
+relative = Path("")
+absolute = relative.absolute()
+print (absolute)
+df = pd.read_csv(str(absolute)+"/workspace/new/newola/RecordingTestPlan.csv")
+df.to_html(str(absolute)+'/workspace/new/newola/RecordingTestPlan.html')
+fn=codecs.open(str(absolute)+"/workspace/new/newola/RecordingTestPlan.html", 'r')
 m = fn.read()
 #print (m)
-f = open("$JENKINS_HOME/workspace/new/newola/index.html",'r')
+f = open(str(absolute)+"/workspace/new/newola/index.html",'r')
 filedata = f.read() # read contents
 f.close() # closes file
 filedata = filedata.replace("replaceit2", m)
-f = open("$JENKINS_HOME/workspace/new/newola/index.html",'w')
+f = open(str(absolute)+"/workspace/new/newola/index.html",'w')
 f.write(filedata)
 f.close()
